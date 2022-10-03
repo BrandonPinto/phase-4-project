206d511716cccad5d36fb20956f8e0bed159fe4b
Brandon <b.pintony@gmail.com>
2022-10-03 15:02:16 -0400

    some controller stuff added

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+  def index
+    render json: User.all
+  end
+  # def create
+  #   user = User.create!(user_params)
+  #   if user.valid?
+  #     render json: user, status: 202
+  #   else
+  #     render json: { error: 'Not Valid' }, status: :unprocessable_entity
+  #   end
+  # end
+  # def show
+  #   user = User.find_by(id: session[:user_id])
+  #   if user
+  #     render json: user
+  #   else
+  #     render json: {error: 'Not Authorized'}, status: 404
+  #   end
+  # end
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+  def show
+    @current_user = User.find(session[:user_id])
+    render json: @current_user
+  end
+#   def update
+#     user = User.find_by(id: session[:user_id])
+#     if user.update(patch_params)
+#       render json: user, status: :created
+#     else
+#       render json: {error: user.errors.full_messages}
+#     end
+#   end
+  private
+  def user_params
+    params.permit(:email, :username, :password)
+  end
+#   def patch_params
+#     params.permit(:funds)
+#   end
+end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sessions
   resources :reposts
   resources :saved_links
   resources :profiles
