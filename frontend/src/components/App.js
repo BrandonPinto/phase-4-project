128e4cb8f6eeadae7eddee1786af297d000f8cf7
George Baroi <baroi.george@gmail.com>
2022-10-06 00:22:48 -0400

    the frontend is done but routers dont work

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,15 +5,38 @@ import Write from "../pages/write/Write";
 import TopBar from "./Topbar/TopBar";
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link, Routes } from "react-router-dom";
 
  
 
 function App() {
+  const user = false;
   return (
-    <>
-    <TopBar />
-    <Register />
-    </>
+    <Router>
+      <TopBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home/> : <Register />}</Route>
+        <Route path="/login">{user ? <Home/> : <Login />}</Route>
+        <Route path="/write">{user ? <Write/> : <Register />}</Route>
+        <Route path="/settings">{user ? <Settings/> : <Register />}</Route>
+        <Route path="/post/:postId">
+        
+         
+        
+          
+        
+          
+        
+          <Single/>
+        </Route>
+      </Switch>
+
+
+    </Router>
   )
 }
 
